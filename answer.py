bbefb73f50c377ee83f522c04df32d5300546a5c
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-03-31 11:07:00 -0500

    Solución Ejercicio Antonov

--- answer.py
+++ answer.py
@@ -1,10 +1,11 @@
 acum=0
-limit= 250000*0.95
-limit_T=250000
+limit= 250000*0.95        
+limit_T=250000           
 
 while acum < limit:
     peso = int(input("Ingrese el peso del paquete a enviar, en kilogramos:  "))
     
+    #Evitar que reciba el proximo paquete, si este llegara a superar la capicidad maxima del avión.
     if acum + peso > limit_T:
         print("No podemos aceptar su paquete, pues superará el límite.")
         continue 
@@ -17,13 +18,14 @@ while acum < limit:
         ds = int(input("Ingrese la distancia del viaje en kilómetros:  "))
         tv= input("¿El viaje es nacional o intercontinental?\n")
 
+        #Evitar error del tipo de viaje
         if (tv != "nacional") or (tv != "intercontinental"):
             print("Error al ingresar el tipo de viaje.")
             while (tv != "nacional") and (tv != "intercontinental"):
                 tv=input("Por favor vuelva a ingresar el tipo de vuelo.\n")
-
         tv = tv.lower
 
+        #Evaluar condiciones y hallar el precio del envío
         if (tv == "nacional") and (peso > 100):
             precio = (peso*4500+ds*4000) * 0.85
         elif tv == "nacional":
@@ -36,4 +38,4 @@ while acum < limit:
     print("El costo del envío es de $",precio,"COP") 
     acum = peso + acum
 
-print("No se pueden recibir más paquetes")
+print("No se pueden recibir más paquetes.")
