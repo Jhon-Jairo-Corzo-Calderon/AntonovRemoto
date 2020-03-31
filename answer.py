4cb5c1295228396b33b386cda3822454f8079613
Jhon-Jairo-Corzo-Calderon <62897139+Jhon-Jairo-Corzo-Calderon@users.noreply.github.com>
2020-03-31 11:01:20 -0500

    Fix error de tipo de vuelo y otros errores de sintaxis. Ejercicio Antonov

---
+++ answer.py
@@ -0,0 +1,39 @@
+acum=0
+limit= 250000*0.95
+limit_T=250000
+
+while acum < limit:
+    peso = int(input("Ingrese el peso del paquete a enviar, en kilogramos:  "))
+    
+    if acum + peso > limit_T:
+        print("No podemos aceptar su paquete, pues superará el límite.")
+        continue 
+    else:
+
+        if peso < 10:
+            print("El peso no es aceptable")
+            continue
+        
+        ds = int(input("Ingrese la distancia del viaje en kilómetros:  "))
+        tv= input("¿El viaje es nacional o intercontinental?\n")
+
+        if (tv != "nacional") or (tv != "intercontinental"):
+            print("Error al ingresar el tipo de viaje.")
+            while (tv != "nacional") and (tv != "intercontinental"):
+                tv=input("Por favor vuelva a ingresar el tipo de vuelo.\n")
+
+        tv = tv.lower
+
+        if (tv == "nacional") and (peso > 100):
+            precio = (peso*4500+ds*4000) * 0.85
+        elif tv == "nacional":
+            precio=(peso*4500+ds*4000)
+        elif (tv == "intercontinental") and (ds > 8000) and (peso > 400):
+            precio=(peso*4500+(ds*0.6213) *8000) *0.9
+        else:
+            precio=(peso*4500+(ds*0.6213) *8000)
+    
+    print("El costo del envío es de $",precio,"COP") 
+    acum = peso + acum
+
+print("No se pueden recibir más paquetes")
